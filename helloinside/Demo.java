a09cec6cd013d988f20a5c31a76441bdd5f03d76
divyansh-bhardwaj-01 <divyanshb240@gmail.com>
2026-01-16 00:30:30 +0530

    folder

---
+++ helloinside/Demo.java
@@ -0,0 +1,16 @@
+public class Demo2{
+public static void main(string[] args){
+System.out.println("Helloworld");
+System.out.println("Hello Ishant");
+System.out.println("Hello web");
+<<<<<<< HEAD
+}
+
+}
+=======
+System.out.println("Hello web3git");
+
+}
+
+} 
+>>>>>>> main
